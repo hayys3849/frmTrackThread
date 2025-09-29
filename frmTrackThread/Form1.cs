851e9c766c9a097f2c4bea14951b62a8618845bc
hayys3849 <ericktuazondulalia@gmail.com>
2025-09-29 19:35:02 +0800

    THREAD START

--- frmTrackThread/Form1.cs
+++ frmTrackThread/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,37 @@ namespace frmTrackThread
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread2);
+            Thread threadC = new Thread(MyThreadClass.Thread1);
+            Thread threadD = new Thread(MyThreadClass.Thread2);
+
+            threadA.Name = "ThreadA";
+            threadB.Name = "ThreadB";
+            threadC.Name = "ThreadC";
+            threadD.Name = "ThreadD";
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Priority = ThreadPriority.Highest;
+
+            threadB.Priority = ThreadPriority.Normal;
+
+            threadC.Priority = ThreadPriority.AboveNormal;
+
+            threadD.Priority = ThreadPriority.BelowNormal;
+
+            Thread.Join
+
+        }
     }
-}
+    }
+
+
+
---
+++ frmTrackThread/MyThreadClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace frmTrackThread
+{
+    internal class MyThreadClass
+    {
+
+
+        public static void Thread1()
+        {
+            int y = 0;
+
+            do
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + y);
+                System.Threading.Thread.Sleep(500);
+                y++;
+            } while (y != 2);
+        }
+
+        public static void Thread2()
+        {
+            int y = 0;
+
+            do
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + y);
+                System.Threading.Thread.Sleep(1500); 
+                y++;
+            } while (y != 6);
+        }
+    }
+}
